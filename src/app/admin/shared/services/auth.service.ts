832e0e9969a8426f6ffba527b3fdbd97597a8e68
Трофимов Максим Сергеевич <mstrofimov@www.mts.ru>
2022-05-28 15:27:49 +0300

    added firebase auth

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {User} from "../../../shared/interfaces";
+import {FbAuthResponse, User} from "../../../shared/interfaces";
 import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
+import {tap} from "rxjs/operators";
 
 @Injectable()
 export class AuthService {
@@ -10,23 +12,40 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
-  get token(): string {
-    return '';
+  get token(): string | null {
+    // @ts-ignore
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return <string>localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
-    return this.http.post('', user);
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
 
   logout() {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken() {
+  // TODO: нормально разобраться с типами
 
+  private setToken(resp: FbAuthResponse | null) {
+    if (resp && resp.expiresIn && resp.idToken) {
+      const expDate = new Date(new Date().getTime() + +resp.expiresIn * 1000);
+      localStorage.setItem('fb-token', resp.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
   }
 }
