309659cedcb25d4c5136d3d47370b12963728f10
Трофимов Максим Сергеевич <mstrofimov@www.mts.ru>
2022-05-28 14:19:53 +0300

    added shared module, created auth service and login form

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,10 +6,16 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "./shared/services/auth.service";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule,
     RouterModule.forChild([
       { path: '', component: AdminLayoutComponent, children: [
           { path: '', redirectTo: '/admin/login', pathMatch: 'full' },
@@ -29,7 +35,8 @@ import { EditPageComponent } from './edit-page/edit-page.component';
     DashboardPageComponent,
     CreatePageComponent,
     EditPageComponent
-  ]
+  ],
+  providers: [AuthService]
 })
 
 export class AdminModule {
