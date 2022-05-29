b52819859ce580cafec5c44092a7ec13dd692ae3
Трофимов Максим Сергеевич <mstrofimov@0300nbb09rumd6m.msk.mts.ru>
2022-05-29 22:15:14 +0300

    added dashboard (search is not working yet)

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/services/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  pSub: Subscription;
+
+  constructor(
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAllPosts().subscribe(posts => {
+      this.posts = posts
+    })
   }
 
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
+  remove(id: string | undefined) {
+
+  }
 }
